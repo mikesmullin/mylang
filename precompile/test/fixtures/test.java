0fafff4fc8bab42cb0328474a18d5dd87cd7d830
Mike Smullin <mike@smartbombinteractive.com>
2013-09-19 16:38:14 -0600

    committing static/ compiled dir

--- precompile/test/fixtures/test.java
+++
@@ -1,5 +0,0 @@
-public class AccountExtension extends AbstractExtension
-{
-  private static final String CLASS_NAME = AccountExtension.class.getName();
-  private static final int ACCOUNT_CREATION_THREAD_POOL_SIZE = 2;
-}
